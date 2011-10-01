4f2523d07efb08d82d970f8884a3129f8140275c
Rob Allen <rob@akrabat.com>
2011-10-01 22:52:41 +0100

    tidy up autoloading in Album module

--- modules/Album/Module.php
+++ modules/Album/Module.php
@@ -8,40 +8,23 @@ use InvalidArgumentException,
 
 class Module
 {
-
-    public function init2(ModuleManager $moduleManager)
-    {
-        try {
-            \Zend\Loader\AutoloaderFactory::factory(array(
-                'Zend\Loader\ClassMapAutoloader' => array(
-                    __DIR__ . '/autoload_classmap.php',
-                ),
-                'Zend\Loader\StandardAutoloader' => array(
-                    __NAMESPACE__ => __DIR__
-                ),
-            ));
-        } catch (Exception $e) {
-            LDBG($e);
-            exit;
-        }
-        LDBG(\Zend\Loader\AutoloaderFactory::getRegisteredAutoloaders());exit;
-    }
-
     public function init(ModuleManager $moduleManager)
     {
-        // add a listener to the module manager's init.post event to autoload once
-        // we have loaded all the configs
-        $moduleManager->events()->attach('init.post', function($e) use ($moduleManager)
-            {
-                $is_live_site = $moduleManager->getMergedConfig(false)->is_live_site;
-                if ($is_live_site) {
-                    require __DIR__ . '/autoload_register.php';
-                } else {
-                    $standardAutoloader = new \Zend\Loader\StandardAutoloader();
-                    $standardAutoloader->registerNamespace(__NAMESPACE__, __DIR__);
-                    $standardAutoloader->register();
-                }
-            });
+        $this->initAutoloader();
+    }
+
+    public function initAutoloader()
+    {
+        \Zend\Loader\AutoloaderFactory::factory(array(
+            'Zend\Loader\ClassMapAutoloader' => array(
+                __DIR__ . '/autoload_classmap.php',
+            ),
+            'Zend\Loader\StandardAutoloader' => array(
+                'namespaces' => array(
+                    __NAMESPACE__ => __DIR__
+                ),
+            ),
+        ));
     }
 
     public function getConfig($env = null)
@@ -52,7 +35,10 @@ class Module
         }
         if (!isset($config->{$env})) {
             throw new InvalidArgumentException(sprintf(
-                    'Unrecognized environment "%s" provided to "%s"', $env, __METHOD__
+                'Unrecognized environment "%s" provided to "%s\%s"',
+                $env,
+                __NAMESPACE__,
+                __METHOD__
             ));
         }
 
