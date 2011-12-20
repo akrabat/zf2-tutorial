25460071557b633564ea88aadb5872dea688c9c6
Rob Allen <rob@akrabat.com>
2011-12-20 22:36:16 +0000

    adding album module

---
+++ module/Album/src/Album/Controller/AlbumController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Album\Controller;
+
+use Zend\Mvc\Controller\ActionController;
+
+class AlbumController extends ActionController
+{
+    public function indexAction()
+    {
+    }
+
+    public function addAction()
+    {
+    }
+
+    public function editAction()
+    {
+    }
+
+    public function deleteAction()
+    {
+    }
+}
