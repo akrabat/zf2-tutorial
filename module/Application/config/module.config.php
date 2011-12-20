d57b3ab57e5fbb28cdf86337b60c209a85b25a57
Rob Allen <rob@akrabat.com>
2011-12-20 23:43:33 +0000

    updates

--- module/Application/config/module.config.php
+++ module/Application/config/module.config.php
@@ -41,7 +41,7 @@ return array(
             'options' => array(
                 'route'    => '/',
                 'defaults' => array(
-                    'controller' => 'index',
+                    'controller' => 'album',
                     'action'     => 'index',
                 ),
             ),
